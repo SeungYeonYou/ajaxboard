438a9ba9c788d173f66fe603b73275c8b36978a2
izuzero <izuzero@gmail.com>
2014-07-30 22:59:31 +0900

    알림 메시지 기본값 처리 #22

--- m.skins/default/js/common.js
+++ m.skins/default/js/common.js
@@ -12,6 +12,7 @@
 			parent.registerPlugin(this);
 			
 			var triggers = [
+				[ "events.connect",        "after",  this.triggerConnect        ],
 				[ "events.notice",         "before", this.triggerNotice         ],
 				[ "events.noticeOfServer", "before", this.triggerNotice         ],
 				[ "events.notifyDocument", "before", this.triggerNotifyDocument ],
@@ -21,6 +22,21 @@
 			for (var i = 0; i < triggers.length; i++)
 			{
 				parent.insertTrigger(triggers[i][0], triggers[i][1], triggers[i][2]);
+			}
+		},
+		triggerConnect: function()
+		{
+			if (!this.skin_info.push_notify_text)
+			{
+				this.skin_info.push_notify_text = "관리자가 <strong>%MESSAGE%</strong>(이)라고 알림을 보냈습니다.";
+			}
+			if (!this.skin_info.document_notify_text)
+			{
+				this.skin_info.document_notify_text = "<strong>새 글이 있습니다.</strong><p>%TITLE% - %NICKNAME%</p>";
+			}
+			if (!this.skin_info.comment_notify_text)
+			{
+				this.skin_info.comment_notify_text = "<strong>새 댓글이 있습니다.</strong><p>%CONTENT% - %NICKNAME%</p>";
 			}
 		},
 		triggerNotice: function(message)
